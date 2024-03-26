ac56c728bc5edecbadc09600d72c3412cbf02d24
Krotovich Krot <for.games.straus3@gmail.com>
2024-03-26 18:50:32 +0300

    Added constructors

--- UniversityOOPExampleProject/Student.h
+++ UniversityOOPExampleProject/Student.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Student {
@@ -7,4 +8,36 @@ public:
 	string name;
 	int age;
 	double mark;
+	
+	// Default constructor
+	Student() {
+		//cout << "Default constructor is called" << endl;
+		name = "No name";
+		age = 5;
+		mark = 4;
+	}
+
+	// Canonical constructor with parameters (arguments)
+	Student(string nm, int a, double m) {
+		//cout << "Canonical constructor is called" << endl;
+		name = nm;
+		age = a;
+		mark = m;
+	}
+
+	// Constructor with parameters (arguments)
+	Student(string nm) {
+		//cout << "Constructor with parameters is called" << endl;
+		name = nm;
+		age = 0;
+		mark = 0;
+	}
+	
+	string convert() {
+		string s = "";
+		s += name;
+		s += ": age = " + to_string(age);
+		s += ", mark = " + to_string(mark);
+		return s;
+	}
 };
