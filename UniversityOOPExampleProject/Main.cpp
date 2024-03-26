d2c0f4b871f7a9f16988c2c745e84ae4cb795d57
Krotovich Krot <for.games.straus3@gmail.com>
2024-03-26 18:24:00 +0300

    Add project files.

---
+++ UniversityOOPExampleProject/Main.cpp
@@ -0,0 +1,24 @@
+#include "Student.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	Student st;
+	Student st2;
+	Student st3;
+
+	st.name = "Alex";
+	st.age = 14;
+	st.mark = 9;
+
+	st2.name = "Alice";
+	st2.age = 13;
+	st2.mark = 7;
+
+	st3.name = "Potter";
+	st3.age = 15;
+	st3.mark = 10;
+
+	return 0;
+}
