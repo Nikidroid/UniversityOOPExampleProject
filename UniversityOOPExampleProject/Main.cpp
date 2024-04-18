f2edf9cbd71f709fc418b4992015775dcff3ca06
Krotovich Krot <for.games.straus3@gmail.com>
2024-04-18 18:36:53 +0300

    Test of our groups

--- UniversityOOPExampleProject/Main.cpp
+++ UniversityOOPExampleProject/Main.cpp
@@ -1,12 +1,39 @@
 #include "Manager.h"
+#include "Group.h"
 #include <iostream>
 
 int main() {
-	Student st1();
-	Student st2();
+	Group groupA;
+	Group groupB;
+	
+	Student st1{ "Alex", 16, 8 };
+	Student st2{ "Tolya", 15, 7 };
+	Student st3{ "Vanya", 14, 10 };
 
-	//Student* st2 = new Student();
-	//delete st2;
+	groupA.add(st1);
+	groupA.add(st2);
+	groupA.add(st3);
+
+	Student st4{ "Sergey", 16, 8 };
+	Student st5{ "Kirill", 15, 9 };
+	Student st6{ "Vlad", 15, 9 };
+	Student st7{ "Misha", 14, 6 };
+
+	groupB.add(st4);
+	groupB.add(st5);
+	groupB.add(st6);
+	groupB.add(st7);
+
+
+
+	cout << groupA.getInfo() << endl;
+	cout << groupB.getInfo() << endl;
+
+	Manager manager;
+
+	cout << manager.calcAvgMark(groupA) << endl;
+	cout << manager.calcAvgMark(groupB) << endl;
+
 
 	return 0;
 }
