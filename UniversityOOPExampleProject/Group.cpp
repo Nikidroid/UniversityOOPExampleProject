aae77e1c3899f7a2fe0082cd10d5ffa741b092b4
Krotovich Krot <for.games.straus3@gmail.com>
2024-04-23 18:17:01 +0300

    Update class "Group". Its changed to dynamic (heap) memory.

--- UniversityOOPExampleProject/Group.cpp
+++ UniversityOOPExampleProject/Group.cpp
@@ -5,14 +5,24 @@ int Group::getSize() {
 	return size;
 }
 
-bool Group::add(Student student) {
-	if (size < 30) {
-		list[size] = student;
-		size++;
-		return true;
+void Group::add(Student student) {
+	if (size == 0) {
+		list = new Student[1];
+		list[0] = student;
 	}
-	
-	return false;
+	else {
+		Student* temp = new Student[size + 1];
+
+		for (int i = 0; i < size; i++) {
+			temp[i] = list[i];
+		}
+
+		temp[size] = student;
+		delete[] list;
+		list = temp;
+	}
+
+	size++;
 }
 
 // bool Group::remove(Student student){
